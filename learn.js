f1726d80282d2ee6e6b6f156d379111f8b908241
aalok2004 <raghuvanshiaalok@gmail.com>
2026-07-04 21:28:35 +0530

    basics

---
+++ learn.js
@@ -0,0 +1,76 @@
+// *
+// **
+// ***
+// ****
+
+// for (let i = 0; i < 6; i++) {
+//     let str = "";
+//     for (let j = 0; j < i; j++) {
+//         str = str + "*";
+//     }
+//     console.log(str);
+// }
+
+// for (let i = 0; i < 6; i++) {
+//     let str = "";
+//     for (let j = 6; j > i; j--) {
+//         str = str + "*";
+//     }
+//     console.log(str);
+// }
+
+
+for (let i = 0; i < 6; i++) {
+    let str = "";
+    for (let j = 0; j < 5-i; j++) {
+        str = str + " ";
+
+    }
+    console.log(str)
+    for(let z=0;z<i;z++){
+        str=str+"*";
+    }
+    console.log(str);
+    
+}
+
+
+// for (let i = 0; i < 6; i++) {
+//     let str = "";
+//     for (let j = 0; j < i; j++) {
+//         str = str + " ";
+
+//     }
+//     for(let z=0;z<5-i;z++){
+//         str=str+"*";
+//     }
+//     console.log(str);
+    
+// }
+
+// for (let i = 0; i < 6; i++) {
+//     let str = "";
+//     for (let j = 0; j < 5-i; j++) {
+//         str = str + " ";
+
+//     }
+//     for(let z=0;z<i;z++){
+//         str=str+" *";
+//     }
+//     console.log(str);
+    
+// }
+// for (let i = 0; i < 6; i++) {
+//     let str = "";
+//     for (let j = 0; j < i; j++) {
+//         str = str + " ";
+
+//     }
+//     for(let z=0;z<4-i;z++){
+//         str=str+" *";
+//     }
+//     console.log(str);
+    
+// }
+
+
