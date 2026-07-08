b837e03a95c015a194e9e26d4d761cf6f30e64f0
aalok2004 <raghuvanshiaalok@gmail.com>
2026-07-08 22:20:42 +0530

    star pattern

--- pattern3.js
+++ pattern3.js
@@ -4,20 +4,20 @@
 //   ***
 //  ****
 // *****
-// for(let i=0;i<6;i++)
-//     {
-//         let str="";
-//         for(let j=0;j<5-i;j++)
-//             {
-//                 str=str+" ";
-//             }
+for(let i=0;i<6;i++)
+    {
+        let str="";
+        for(let j=0;j<5-i;j++)
+            {
+                str=str+" ";
+            }
             
             
-//         for(let z=0;z<i;z++)
-//             {
-//              str=str+"*"
-//             }
-//             console.log(str);
+        for(let z=0;z<i;z++)
+            {
+             str=str+"*"
+            }
+            console.log(str);
            
 
-// }
+}
