08a5d514d0cc1d4c78ee51c09aaf4c222ed460d2
aalok2004 <raghuvanshiaalok@gmail.com>
2026-07-12 09:39:47 +0530

    star pattern

--- pattern4.js
+++ pattern4.js
@@ -5,16 +5,16 @@
 //    **
 //     *
      
-// for(let i=0;i<6;i++)
-//     {
-//         let str="";
-//         for(let j=5;j>5-i;j--)
-//             {
-//                 str=str+" ";
-//             }
-//         for(let z=5;z>i;z--)
-//             {
-//                 str=str+"*";
-//             }
-//             console.log(str);
-//     }
+for(let i=0;i<6;i++)
+    {
+        let str="";
+        for(let j=5;j>5-i;j--)
+            {
+                str=str+" ";
+            }
+        for(let z=5;z>i;z--)
+            {
+                str=str+"*";
+            }
+            console.log(str);
+    }
